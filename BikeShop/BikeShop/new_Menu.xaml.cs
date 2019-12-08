4b6474d425ce08be702e1d6f5c44dfb57d902e12
tjd5526 <tjd5526@naver.com>
2019-12-08 20:45:56 +0900

    ~80 책에선 NavigationService.Navigate 함수를 통해 화면 Navigate를 시도했는데 현재는 불가능하다 그래서 먼저 Button에 x:Name="contact"를 지정해 줬고 GetNavigationService를 통해 contact를 생성자로 넘겨 성공이 가능했다. 처음엔 stackoverflow를 보면서 고쳤기 때문에 this를 사용했으나 이 this는 Menu의 this로 DependencyObject로 변환이 불가능하다. + DependencyObject 조금더 깊게 파보기

---
+++ BikeShop/BikeShop/new_Menu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BikeShop
+{
+    /// <summary>
+    /// new_Menu.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class new_Menu : Page
+    {
+        public new_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void contact_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(contact);
+            ns.Navigate(new Uri("/Contact.xaml", UriKind.Relative));
+        }
+    }
+}
